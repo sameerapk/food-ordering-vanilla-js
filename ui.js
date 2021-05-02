e539cd45d9a4d23aa19b1a09b497fe41d291b72e
sameerapk <sameerakmurthy@gmail.com>
2021-05-02 16:27:59 +0530

    added buttons for pafination and some css changes

--- ui.js
+++ ui.js
@@ -7,10 +7,12 @@ class UI {
                 <div class='img-container' id=${restaurant.id}>
                     <img src="${restaurant.image}" alt ='restaurant' class='restaurant-img' />
                 </div>
-                <h3>${restaurant.name}</h3>
-                <h4>Location - ${restaurant.location}</h4>
-                <h4>Ratings - ${restaurant.rating}</h4>
-                <h4>ETA - ${restaurant.ETA}</h4>
+                <div class='res-content'>
+                    <h3>${restaurant.name}</h3>
+                    <h4>Location - ${restaurant.location}</h4>
+                    <h4>Ratings - ${restaurant.rating}</h4>
+                    <h4>ETA - ${restaurant.ETA}</h4>
+                </div>
             </div>
             `
         })
